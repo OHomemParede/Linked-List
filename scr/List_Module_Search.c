f252d3aabb07fb0d0a3931872be8a8b944a73cdf
Daniel de Souza Lima Junior <39025412+OHomemParede@users.noreply.github.com>
2021-08-11 21:43:17 -0300

    Update List_Module_Search.c

---
+++ scr/List_Module_Search.c
@@ -0,0 +1,12 @@
+#include "../include/List_Core.h"
+#include "../include/List_Module_Search.h"
+
+
+List *List_find(CONTENT_TYPE n, List *k){
+    while(k != _NULL_){
+        if(k->content == n)
+            return k;
+        else
+            k = k->next;
+    }
+}
