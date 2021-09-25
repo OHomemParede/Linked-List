eda570b5b52622e174e37f75edb70f5ac22e5e49
Daniel de Souza Lima Junior <Dewaldman@outlook.com>
2021-09-25 15:51:54 -0300

    Docs: better docs for core.h

--- include/core.h
+++ include/core.h
@@ -26,28 +26,28 @@ List* List_init(CONTENT_TYPE n);
 
 
 /**
- * Create a new List struct with content `n` and add it to begining of the list.
- * Returns the new List struct pointing out to node.
+ * Create a new List struct with content <n> and add it to begining of the list.
+ * Returns the new List struct address pointing out to <node>.
  */
 List* List_append(CONTENT_TYPE n, List* node);
 
 
 /**
- *  Remove node from list k. 
- *  If node == k, returns a pointer to k->next and remove node.
- *  Else: returns the head of the list k
+ *  Remove <node> from list <k>. 
+ *  if <node> == <k>, returns `k->next`.
+ *  else, returns <k>.
  */
 List* List_remove(List* node, List* k);
 
 
 /**
- *  Update the content of a given node.
+ *  Update the content of a given <node> with <n>.
  */
 void List_update(CONTENT_TYPE n, List* node);
 
 
 /**
- * Print node's content to stdout until (node.next == NULL)
+ * Print <node> content to stdout until `node->next == NULL`
  */ 
 void List_print(List* node);
 
